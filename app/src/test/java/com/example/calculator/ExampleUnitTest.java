093370e3d3580bc1e2ffa13091956863c1517fc2
ANDRES <afcortes1@hotmail.com>
2023-04-24 21:43:37 -0500

    parcial 2 calculadora

---
+++ app/src/test/java/com/example/calculator/ExampleUnitTest.java
@@ -0,0 +1,30 @@
+package com.example.calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void multiply_isCorrect() {
+        assertEquals(4, 2 * 2);
+    }
+
+    @Test
+    public void division_isCorrect() {
+        assertEquals(1, 2 / 2);
+    }
+    @Test
+    public void subtraction_isCorrect() {
+        assertEquals(0, 2 - 2);
+    }
+}
